af57650be928c0d93d69bf8282f13a6a650cdd3b
Piyush Arora <piyusharora1504@gmail.com>
2025-04-10 17:29:09 +0530

    completed routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import SignInPage from './auth/sign-in/index.jsx'
+import Home from './home/index.jsx'
+import Dashboard from './dashboard/index.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />
+      }
+    ]
+  },
+  {
+    path: '/auth/sign-in',
+    element: <SignInPage/>
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>,
 )
