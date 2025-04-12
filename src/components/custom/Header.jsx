8b71f260deaf48f45cae8a30891c08b9140a199e
Piyush Arora <piyusharora1504@gmail.com>
2025-04-12 21:56:01 +0530

    link to dashboard and sign-in page

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
+import { UserButton, useUser } from '@clerk/clerk-react'
 const Header = () => {
+  const {user, isSignedIn} = useUser();
   return (
-    <div className='p-3 px-5' flex justify-between shadow-md>
+    <div className='p-3 px-5 flex justify-between shadow-md'>
       <img src="/logo.svg" width={100} height={100} />
-
-      <Button>Get Started</Button>
+      {isSignedIn ?
+      <div className='flex gap-2 items-center'>
+        <Link to={'/dashboard'}>
+        <Button variant="outline">Dashboard</Button>
+        </Link>
+        <UserButton />
+        </div> :
+        <Link to={'/auth/sign-in'}>
+            <Button>Get Started</Button>
+        </Link>
+        }
     </div>
   )
 }
